d3d773598b93a2a955fb44314fd87e158ea4ae1d
BrianDinh26 <briandinh2022@u.northwestern.edu>
2023-11-17 12:37:04 -0600

    Basic import cleaning work. gg_mis_var not working despite library import?

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,11 +1,23 @@
-#data messing around
+#dataset
 #https://www.kaggle.com/datasets/tianhwu/brooklynhomes2003to2017/data?select=brooklyn_sales_map.csv
 
+#load packages
 library(tidyverse)
 library(naniar)
+library(janitor)
 
-brooklyn_sales <- read_csv("data/brooklyn_sales_map.csv")
+#load dataset, also changing a lot of character NA values to actual NA values.
+#also clean column names
+brooklyn_sales <- read_csv("data/brooklyn_sales_map.csv", na = c("NA", "")) |> 
+  clean_names()
 
+#checking some missingness
+gg_mis_var(brooklyn_sales)
+
+
+
+
+#older code that I may or may or may not use.
 brooklyn_sales <- subset(brooklyn_sales, select = -c(borough, Borough, easement, apartment_number))
 
 unique(brooklyn_sales$ZipCode)
